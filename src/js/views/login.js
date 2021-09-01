1fb34710ad6947dc62d26ef22c1d10a836671a10
olaracode <octavioasapchi@gmail.com>
2021-09-01 06:00:44 -0400

    Login en proceso

--- src/js/views/login.js
+++ src/js/views/login.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Dog from "./../../img/dog.png";
 export const Login = () => (
-	<div className="text-center mt-5">
-		<h1>Login/Registrar</h1>
+	<div className="vh-100 mt-5">
+		<div className="row w-75 mt-5 h-75 card mx-auto loginForm">
+			<div className="text-center flex-column imagen col-6 p-5 h-100 my-auto">
+				<p>Conviertete en un guardian permanente o consigue un hogar para un perro</p>
+				<div className="flex-fill align-self-center">
+					<img className="w-50" src={Dog} />
+				</div>
+			</div>
+		</div>
 	</div>
 );
